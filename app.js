c63ae8a8f01726da7af2b1439789c0f4da20b7fb
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-01-15 19:55:04 +0800

    Adding a middleware and bodyparser json format

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+
+//routes
 const articlesRoute = require('./routes/articles');
 
+//middlewares
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+//application routes
 app.use(articlesRoute);
 
+//ports
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });

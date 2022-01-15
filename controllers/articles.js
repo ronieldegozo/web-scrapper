7352caa9a92ac7fdf1ebaa0aa74dd6de9d18da70
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-01-15 20:09:34 +0800

    Changing API Promises to Async/Await

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,35 +5,34 @@ const axios = require('axios');
 let url = 'https://www.inquirer.net/';
 let url2 = axios(url);
 
-exports.getArticles = (req, res) => {
-    url2
-        .then((result) => {
-            const html = result.data; //DATA FROM URL
-            const datas = cheerio.load(html); //LOAD DATA INTO CHEERIO
+exports.getArticles = async (req, res) => {
 
-            const articles = [];
-            datas('#tr_boxs3', html).each(function(){
-                const title = datas(this).find('a').text();
-                const link = datas(this).find('a').attr('href');
-                const date = datas(this).find('h6 span').text();
-                const category = datas(this).find('h6').text();
+    try {
+        const url1 = await url2;
+        const html = url1.data;
+        const datas = cheerio.load(html); //LOAD DATA FROM CHEERIO
+        const articles = [];
+        datas('#tr_boxs3', html).each(function(){
+            const title = datas(this).find('a').text();
+            const link = datas(this).find('a').attr('href');
+            const date = datas(this).find('h6 span').text();
+            const category = datas(this).find('h6').text();
 
-                // datas('#tr_boxs3').find(datas('h6')).text();
-                // const category = datas(this).find('h6').text();
-
-                console.log(category);
-
-                articles.push({
-                    title,
-                    link,
-                    date,
-                    category
-                });
-        
-            })
-            res.json(articles);
+            articles.push({
+                title,
+                link,
+                date,
+                category
+            });
+    
         })
-        .catch((err) => {
-            console.log(err)
-        })     
+        res.status(201).json({
+            status: 'Articles fetched',
+            articles
+        })
+
+    }catch(err){
+        console.log(err)
+    }
+
 };
